d63d06d3dbae75b324d749e69dd288b14af69076
SHIHAB KHAN <41753677+shihab0005@users.noreply.github.com>
2018-10-24 11:42:09 -0700

    Update 6.sentence_revers.py

--- 6.sentence_revers.py
+++ 6.sentence_revers.py
@@ -1,3 +1,16 @@
-
+'''
 str = input("Enter A sentence :")
 print (' '.join(str.split()[::-1]))
+'''
+
+def reverse(string) :
+    string = string.strip()
+    result = [word[::-1] for word in string[::-1].split(" ")]
+    revers = " ".join(result)
+    print("The reverse sentence is :",revers)
+
+
+
+string = input("Enter A sentence :")
+print("The sentence is : ", string)
+reverse(string)
